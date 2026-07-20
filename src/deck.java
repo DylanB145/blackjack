965e750499c305627605dbddaa9d3245bb0c987e
Dylan Brooks <thedylanb145@outlook.com>
2026-07-20 20:36:35 +0000

    Add deck constructor and change cards to an arraylist

--- src/deck.java
+++ src/deck.java
@@ -1,3 +1,43 @@
+import java.util.ArrayList;
+
 public class deck {
-    
+
+    ArrayList<card> cards;
+
+    deck() {
+        cards = new ArrayList<card>();
+        for (int deck = 0; deck < 3; ++deck) {
+
+            for (int suit = 0; suit < 4; ++suit) {
+                card.suitEnum tempEnum = card.suitEnum.Clubs;
+                for (int value = 2; value <= 10; ++value) {
+
+                    switch (suit) {
+                        case 0:
+                            tempEnum = card.suitEnum.Spades;
+                            break;
+                        case 1:
+                            tempEnum = card.suitEnum.Hearts;
+                            break;
+                        case 2:
+                            tempEnum = card.suitEnum.Diamonds;
+                            break;
+                        case 3:
+                            tempEnum = card.suitEnum.Clubs;
+                            break;
+
+                        default:
+                            System.err.println("invalid card suit creation");
+                            tempEnum = card.suitEnum.Clubs;
+                            break;
+                    }
+                    cards.add(new card(value, Integer.toString(value), tempEnum));
+                }
+                cards.add(new card(10,"J",tempEnum));
+                cards.add(new card(10,"Q",tempEnum));
+                cards.add(new card(10,"K",tempEnum));
+                cards.add(new card(11,"A",tempEnum));
+            }
+        }
+    }
 }
