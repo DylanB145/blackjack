eb8bc6c31be33d5bce30c59bd89afdd52d761201
Dylan Brooks <thedylanb145@outlook.com>
2026-07-21 15:48:54 +0000

    Add dealer class

--- src/dealer.java
+++ src/dealer.java
@@ -1,3 +1,18 @@
 public class dealer {
-    
+    private hand dealerHand;
+    dealer(){
+        dealerHand = new hand();
+    }
+    void newRound(){
+        dealerHand = new hand();
+    }
+    void addCard(card aCard){
+        dealerHand.addCard(aCard);
+    }
+    int getHandValue(){
+        return dealerHand.getValue();
+    }
+    String getHandToString(){
+        return "dealer ".concat(dealerHand.handToString());
+    }
 }
