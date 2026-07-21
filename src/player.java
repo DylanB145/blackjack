75d17de2f802d85e48bf2a9193734f29b1f973ac
Dylan Brooks <thedylanb145@outlook.com>
2026-07-21 15:48:38 +0000

    Add Player Class

--- src/player.java
+++ src/player.java
@@ -1,3 +1,26 @@
 public class player {
-    
+    //variables
+    private hand playerHand;
+    private int gainLoss;
+
+
+    player(){
+        gainLoss=0;
+        playerHand = new hand();
+    }
+    void newRound(){
+        playerHand = new hand();
+    }
+    void addCard(card aCard){
+        playerHand.addCard(aCard);
+    }
+    int getHandValue(){
+        return playerHand.getValue();
+    }
+    int getGainLoss(){
+        return gainLoss;
+    }
+    void addGainLoss(int bet){
+        gainLoss+=bet;
+    }
 }
